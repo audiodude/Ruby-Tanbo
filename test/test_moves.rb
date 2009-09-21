189d1e52eb97b46dbac8fc6140c8331ed334da89
Travis Briggs <briggs.travis@gmail.com>
2009-09-20 22:31:04 -0400

    Remove testing debug put statements

--- test/test_moves.rb
+++ test/test_moves.rb
@@ -20,18 +20,9 @@ class MovesTest < Test::Unit::TestCase
   end
 
   def test_move_effects
-    require 'pp'
-    
-    puts "="*10
-    pp @main_controller.roots
-    puts "="*10
-    
     point = @main_controller.get_board[11, 12]
     assert @main_controller.valid_move?(point), ("Expected BLACK valid move reported invalid at: " + point.inspect)
     @main_controller.make_move(point, @main_controller.get_board[12, 12])
-    # puts "="*10
-    # pp @main_controller.roots
-    # puts "="*10
     assert_equal TanboBoard::BLACK, @main_controller.get_color(point), ("Point should be BLACK after single black move at: " + point.inspect)
     assert ! @main_controller.valid_move?(point, TanboBoard::BLACK), ("Move reported valid for BLACK after already being made at: " + point.inspect)
     assert ! @main_controller.valid_move?(point, TanboBoard::WHITE), ("Move reported valid for WHITE after already being made at: " + point.inspect)
