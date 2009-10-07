9b84eb0b562bb5618ae1d7f60a87971ac2e5934a
Travis Briggs <briggs.travis@gmail.com>
2009-10-07 03:43:46 -0400

    Automatically generate the text INSTALL file from the .textile, using http://www.aaronsw.com/2002/html2text/

--- doc/Rakefile.rb
+++ doc/Rakefile.rb
@@ -1,21 +1,41 @@
+require 'pathname'
+
+cwd = Pathname.new(Dir.pwd)
+#$: << cwd.parent
 require 'redcloth'
+require 'hpricot'
 require 'rake/clean'
 
 INSTALL_TEXTILE = 'installation_instructions.textile'
 INSTALL_HTML = INSTALL_TEXTILE.ext('html')
+INSTALL = cwd.parent.join('INSTALL')
 
 CLEAN.include(INSTALL_HTML)
+CLOBBER.include(INSTALL)
 
-task :doc => INSTALL_HTML
+def generate_html
+  source = File.new(INSTALL_TEXTILE).read
+  RedCloth.new(source).to_html
+end
+
+task :doc => [INSTALL, INSTALL_HTML]
 task :default => :doc
 
 file INSTALL_HTML => INSTALL_TEXTILE do
-  source = File.new(INSTALL_TEXTILE).read
-  output = File.new(INSTALL_HTML, 'w')
   puts "Generating #{INSTALL_HTML} from #{INSTALL_TEXTILE}..."
-  output.print(RedCloth.new(source).to_html)
+  output = File.new(INSTALL_HTML, 'w')
+  output.print(generate_html)
+  output.close
   puts "Done!"
 end
 
+file INSTALL => INSTALL_HTML do
+  puts "Generating #{INSTALL} from #{INSTALL_TEXTILE}..."
+  output = File.new(INSTALL, 'w')
+  text = `python html2text.py #{INSTALL_HTML}`
+  output.print(text.gsub(/\n\[!(\[?\d?\]?)+\s*/, ''))
+  output.close
+  puts "Done!"
+end
 
 
